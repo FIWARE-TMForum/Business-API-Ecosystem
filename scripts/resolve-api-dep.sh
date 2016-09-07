b58073b9eb4d567befe50fb8bc5e100ffd25be4e
fdelavega <fdelavega@conwet.com>
2016-09-07 12:45:32 +0200

    Include missing -y flags in installation scripts

--- scripts/resolve-api-dep.sh
+++ scripts/resolve-api-dep.sh
@@ -3,8 +3,8 @@
 # Install Java
 add-apt-repository ppa:webupd8team/java
 apt-get update
-apt-get install oracle-java8-installer
-apt-get install oracle-java8-set-default
+apt-get install -y oracle-java8-installer
+apt-get install -y oracle-java8-set-default
 
 # Install Glassfish
 cd /opt/biz-ecosystem/
@@ -24,6 +24,6 @@ tar -xvf mysql-connector-java-5.1.39.tar
 cp mysql-connector-java-5.1.39/mysql-connector-java-5.1.39-bin.jar glassfish4/glassfish/lib
 
 # Install maven
-apt-get install maven
+apt-get install -y maven
 
 cd $WORKSPACE
