cdf78f6c45cc92f1b2a1e9fd49006552a7157f67
fdelavega <fdelavega@conwet.com>
2016-12-01 13:07:16 +0100

    Update URL in proxy installer

--- scripts/resolve-proxy-dep.sh
+++ scripts/resolve-proxy-dep.sh
@@ -3,7 +3,7 @@
 # Install node and npm
 cd /opt/biz-ecosystem/
 
-wget https://nodejs.org/dist/v4.5.0/node-v6.9.1-linux-x64.tar.xz
+wget https://nodejs.org/dist/v6.9.1/node-v6.9.1-linux-x64.tar.xz
 tar -xvf node-v6.9.1-linux-x64.tar.xz
 
 cd $WORKSPACE
